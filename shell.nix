d149b10ab5c8ab9eef25f3bd6357beaff21496d8
IP-Cam <cam@industrialplankton.com>
2024-02-25 20:45:33 -0800

    working nix-shell config

---
+++ shell.nix
@@ -0,0 +1,42 @@
+let
+  pkgs = import <nixpkgs> { };
+
+  libraries = with pkgs;[
+    webkitgtk
+    gtk3
+    cairo
+    gdk-pixbuf
+    glib
+    dbus
+    openssl_3
+  ];
+
+  packages = with pkgs; [
+    pkg-config
+    dbus
+    openssl_3
+    glib
+    gtk3
+    libsoup
+    webkitgtk
+    appimagekit
+    nodejs
+    nodePackages.pnpm
+    rustc
+    cargo
+    rustfmt
+    rust-analyzer
+    clippy
+  ];
+in
+pkgs.mkShell {
+  buildInputs = packages;
+
+  shellHook =
+    ''
+      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+      export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+
+      cargo help tauri 2> /dev/null 1> /dev/null || cargo install tauri-cli
+    '';
+}
